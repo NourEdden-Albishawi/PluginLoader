b7f294fd4a98ac6cd8475da99d9c231e6c4956d0
NourEdden-Albishawi <nbishawi@gmail.com>
2025-10-27 17:57:35 +0300

    feat(backend): Initial Gradle build setup
    Configures the Gradle build script for the `loader-backend` module. This setup includes applying the `application` plugin for a runnable JAR, defining dependencies (such as SnakeYAML for configuration parsing, Lombok for boilerplate reduction, and the `loader-common` module), and configuring the `shadowJar` plugin to create a fat JAR for easy deployment of the backend server.

--- loader-backend/build.gradle.kts
+++ loader-backend/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("java")
     id("application")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "${parent?.group}"
@@ -16,4 +19,10 @@ repositories {
 
 dependencies {
     implementation(project(":loader-common"))
+    implementation("org.yaml:snakeyaml:1.33")
+}
+
+// This is needed to include the dependency in the shadowJar
+tasks.withType<ShadowJar> {
+    relocate("org.yaml.snakeyaml", "me.akraml.loader.libs.snakeyaml")
 }
